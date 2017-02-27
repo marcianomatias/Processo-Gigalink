b4ff847bab25a32eaf4b5e89d8d0415585599a5a
marci <marci@DESKTOP-BC0DVRL>
2017-02-26 23:58:18 -0300

    Programa para processo seletivo Gigalink

---
+++ src/programavendas/ProgramaVendas.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programavendas;
+
+import Formularios.Principal;
+
+/**
+ * @author Marciano Matias dos Santos
+ * Projeto Gigalink 
+ * 03/01/2016
+ */
+public class ProgramaVendas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal frm = new Principal();
+        frm.setVisible(true);
+    }
+    
+}
---
+++ src/utilitarios/ConectaBanco.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilitarios;
+
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author marci
+ */
+public class ConectaBanco {
+   public Statement stm;
+   public ResultSet rs;
+   private  String driver = "org.postgresql.Driver";
+   private  String caminho = "jdbc:postgresql://localhost:5433/programavendas";
+   private  String usuario = "postgres";
+   private  String senha = "emcasa12";
+   public Connection conn;
+   
+   public void conexao(){
+      
+       try {
+            System.setProperty("jdbc.Driver", driver);
+           conn = DriverManager.getConnection(caminho, usuario, senha);
+           JOptionPane.showMessageDialog(null, "Conectado com Sucesso ao BD!!!");
+       } catch (SQLException ex) {
+          JOptionPane.showMessageDialog(null, "Erro de conexão ao BD !!! \n Erro:"+ex.getMessage());
+       }   
+   }
+   public void executaSQL(String sql){
+       try {
+           stm = conn.createStatement(rs.TYPE_SCROLL_INSENSITIVE,rs.CONCUR_READ_ONLY);
+           rs = stm .executeQuery(sql);
+       } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "Erro EXECUTA sql !!! \n Erro:"+ex.getMessage());
+       }
+   }
+   public void desconecta(){
+       try {
+           conn.close();
+           JOptionPane.showMessageDialog(null, "Desconectado com Sucesso ao BD!!!");
+       } catch (SQLException ex) {
+          JOptionPane.showMessageDialog(null, "Erro ao encerrar a conexão ao BD !!! \n Erro:"+ex.getMessage());
+       }
+   
+   }
+
+    public void Conexao() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public void Desconecta() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+  }
